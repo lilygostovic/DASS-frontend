79a24887e1d80086444dddb38cce54eec77ed97c
ContonJon <jonathanstenkilde@hotmail.com>
2023-03-03 12:38:39 +0100

    Added Home and Charts buttons to navigate

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,18 +1,38 @@
 import { LanguagePicker } from './LanguagePicker';
+import { NavButton } from './NavButton';
 import React from 'react';
+import { useMatch } from 'react-router-dom';
 
-export const Nav = () => (
+export const Nav = () => {
+  const isHome = useMatch("/");
+  const isCharts = useMatch("/charts");
+
+  return (
     <div style={{
       position: "relative",
       width: "100%",
       height: "8vh",
       display: "flex",
-      justifyContent: "end",
       alignItems: "center"
     }}>
       <div style={{ backgroundColor: "#000000", width: "100%", height: "100%", position: 'absolute', opacity: "0.5", zIndex: "0" }}/>
-      <div style={{ zIndex: 1, padding: "20px" }}>
+      <div style={{ zIndex: 1, padding: "20px", justifyContent: "space-between", width: "100%", display: "flex" }}>
+        <div>
+          <NavButton
+            // className={isHome?"active":""}
+            to="/"
+          >
+            Home
+          </NavButton>
+          <NavButton
+            // className={isCharts?"active":""}
+            to="/charts"
+          >
+            Charts
+          </NavButton>
+        </div>
         <LanguagePicker />
       </div>
     </div>
-);
+  )
+};
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,7 @@
+import {
+  Nav
+} from '../../components';
+
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
@@ -7,13 +11,14 @@ export const Home = () => {
   const text = t("Welcome to The Home of Asylum Seekers");
 
   return (
-      <div style ={{
-        backgroundColor: "#141C27",
-        maxHeight: "100vh",
-        minHeight: "100vh"
-      }}>
-        <div style={{ color: "white", marginBottom: "20px", justifyContent: "center", alignItems: "center", height: "15vh", display: "flex", flexDirection: "column" }}>
-          {text}
-        </div>
-      </div>)
+    <div style ={{
+      backgroundColor: "#141C27",
+      maxHeight: "100vh",
+      minHeight: "100vh"
+    }}>
+    <Nav />
+      <div style={{ color: "white", marginBottom: "20px", justifyContent: "center", alignItems: "center", height: "15vh", display: "flex", flexDirection: "column" }}>
+        {text}
+      </div>
+    </div>)
 }
