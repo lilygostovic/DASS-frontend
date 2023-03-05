b24e9eeec26f13140cb9e850475dc1206714c7d5
Lily Gostovic <lily.gostovic@gmail.com>
2023-03-05 08:51:29 +0100

    Fix linting and remove comments

--- src/components/Nav/NavButton.tsx
+++ src/components/Nav/NavButton.tsx
@@ -1,34 +1,8 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-// export const NavButton = ({ text, isSelected, onClick }: ButtonProps) => {
-//   const textColor = isSelected ? "white" : "grey";
-
-//   return (
-//     <button
-//         onClick={onClick}
-//         style={{
-//           color: `${textColor}`,
-//           backgroundColor: "transparent",
-//           border: "none",
-//           borderRadius: "8px",
-//           margin: "20px"
-//         }}>
-//             {text}
-//     </button>
-//   )
-// }
-
 export const NavButton = styled(Link)`
-    color:white;
+    color: white;
     margin: 10px;
-    textDecoration: 'none'
-
-    
-
-
-
-
-
-
+    text-decoration: none;
 `;
