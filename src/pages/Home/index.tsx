0844afcf4ac7bf4b88146f737770efa9c468251f
ContonJon <jonathanstenkilde@hotmail.com>
2023-03-07 19:32:09 +0100

    Added some possible text sections

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -50,18 +50,46 @@ export const Home = () => {
           >
           <div
             style={{
-              color: "blue",
               backgroundColor: "#ffffff",
               height: "84vh",
               width: "100%",
               border: "4px solid #78A6F5",
             }}
           >
-            PlaceholderText
+            <h1 style={{ fontSize: "19px", color: "#577ACB" }}>
+              About This Project
+              <div style={{ fontSize: "12px", color: "black" }}>
+                Here we could maybe add reasons for why this is the best project ever
+              </div>
+            </h1>
+
+            <h1 style={{ fontSize: "19px", color: "#577ACB" }}>
+              What YOU have to do
+              <div style={{ fontSize: "12px", color: "black" }}>
+                Maybe instructions for what the user can do???
+                What each page shows, and how to operate them?
+              </div>
+            </h1>
+
+            <h1 style={{ fontSize: "19px", color: "#577ACB" }}>
+              Resources
+              <div style={{ fontSize: "12px", color: "black" }}>
+                The website/tools we have used? What kind of data is used?
+              </div>
+            </h1>
+
+            <h1 style={{ fontSize: "19px", color: "#577ACB" }}>
+              Contact Us
+              <div style={{ fontSize: "12px", color: "black" }} >
+                Contact information like emails and some links perhaps?
+                Maybe some info like this could also be placed in the empty bottom right corner?
+                IDK what else could be interesting to place in the empty corner? :DDD
+              </div>
+            </h1>
           </div>
           <div
             style={{
-              height: "85vh",
+              height: "47vh",
               backgroundColor: "#ffffff",
               border: "4px solid #78A6F5",
               width: "50%",
