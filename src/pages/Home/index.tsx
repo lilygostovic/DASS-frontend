5f4bad473024bfed30bb6a3e9e9264d56bcf7e27
ContonJon <jonathanstenkilde@hotmail.com>
2023-03-11 15:15:02 +0100

    json translations are re-formatted

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from "react-i18next";
 export const Home = () => {
   const { t } = useTranslation();
 
-  const text = t("Welcome to The Home of Asylum Seekers");
+  const text = t(`homepage.title`);
 
   return (
     <div style ={{
