ae53c5ba56899a1a8c3f3ee217e4a804f05bacd6
ContonJon <jonathanstenkilde@hotmail.com>
2023-03-11 14:39:37 +0100

    Fixed missing div tags

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,61 +12,58 @@ export const Home = () => {
     <div style ={{
       backgroundColor: "#ECF2FF",
       maxHeight: "100vh",
-      minHeight: "100vh"
+      minHeight: "100vh",
     }}>
-    <Nav />
-      <div style={{ marginBottom: "20px", justifyContent: "center", alignItems: "center", height: "85vh", display: "flex", flexDirection: "column" }}>
-        <div style={{ color: "black", height: "80vh", fontSize: "25px", fontWeight: "bold" }}>{text}</div>
-        <div style={{ backgroundColor: "#ffffff", height: "800vh", width: "150vh", border: "2px solid #3E54AC", justifyContent: "left", display: "flex" }} >
-    <div
-      style={{
-        backgroundColor: "#ECF2FF",
-        maxHeight: "100vh",
-        minHeight: "100vh",
-      }}
-    >
-      <Nav />
       <div
         style={{
-          marginBottom: "20px",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "85vh",
-          display: "flex",
-          flexDirection: "column",
+          backgroundColor: "#ECF2FF",
+          maxHeight: "100vh",
+          minHeight: "100vh",
         }}
       >
+        <Nav />
         <div
           style={{
-            color: "black",
-            height: "80vh",
-            fontSize: "25px",
-            fontWeight: "bold",
+            marginBottom: "20px",
             justifyContent: "center",
-          }}
-        >
-          {text}
-        </div>
-        <div
-          style={{
-            backgroundColor: "#ffffff",
-            height: "800vh",
-            width: "150vh",
-            border: "2px solid #3E54AC",
-            justifyContent: "left",
+            alignItems: "center",
+            height: "85vh",
             display: "flex",
+            flexDirection: "column",
           }}
         >
-          <div>
-            <h1 style={{ fontSize: "20px" }}>About Us</h1>
-            We could have something like this for the home page content with
-            links and such. Just a bad looking idea, you are welcome to do
-            something else :D
+          <div
+            style={{
+              color: "black",
+              height: "80vh",
+              fontSize: "25px",
+              fontWeight: "bold",
+              justifyContent: "center",
+            }}
+          >
+            {text}
+          </div>
+          <div
+            style={{
+              backgroundColor: "#ffffff",
+              height: "800vh",
+              width: "150vh",
+              border: "2px solid #3E54AC",
+              justifyContent: "left",
+              display: "flex",
+            }}
+          >
+            <div>
+              <h1 style={{ fontSize: "20px" }}>About Us</h1>
+              We could have something like this for the home page content with
+              links and such. Just a bad looking idea, you are welcome to do
+              something else :D
+            </div>
           </div>
         </div>
-      </div>
-      <div style={{ alignItems: "flex-end" }}>
-        <Footer />
+        <div style={{ alignItems: "flex-end" }}>
+          <Footer />
+        </div>
       </div>
     </div>
   );
