f1412ad4ac9e5c3f65941d906447e5e7e5e36acc
ContonJon <jonathanstenkilde@hotmail.com>
2023-03-12 10:06:31 +0100

    Refactored home page text into a seperate component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import { Footer, Nav } from "../../components";
 import React, { useState } from "react";
+import { HomeText } from "./components/HomeText";
 import { SummaryChart } from "../Dashboard/components";
 import { data } from "../Dashboard/data";
 import { useTranslation } from "react-i18next";
@@ -48,45 +49,7 @@ export const Home = () => {
               flexDirection: "row",
             }}
           >
-          <div
-            style={{
-              backgroundColor: "#ffffff",
-              height: "84vh",
-              width: "100%",
-              border: "4px solid #78A6F5",
-            }}
-          >
-            <h1 style={{ fontSize: "19px", color: "#577ACB" }}>
-              About This Project
-              <div style={{ fontSize: "12px", color: "black" }}>
-                Here we could maybe add reasons for why this is the best project ever
-              </div>
-            </h1>
-
-            <h1 style={{ fontSize: "19px", color: "#577ACB" }}>
-              What YOU have to do
-              <div style={{ fontSize: "12px", color: "black" }}>
-                Maybe instructions for what the user can do???
-                What each page shows, and how to operate them?
-              </div>
-            </h1>
-
-            <h1 style={{ fontSize: "19px", color: "#577ACB" }}>
-              Resources
-              <div style={{ fontSize: "12px", color: "black" }}>
-                The website/tools we have used? What kind of data is used?
-              </div>
-            </h1>
-
-            <h1 style={{ fontSize: "19px", color: "#577ACB" }}>
-              Contact Us
-              <div style={{ fontSize: "12px", color: "black" }} >
-                Contact information like emails and some links perhaps?
-                Maybe some info like this could also be placed in the empty bottom right corner?
-                IDK what else could be interesting to place in the empty corner? :DDD
-              </div>
-            </h1>
-          </div>
+            <HomeText />
           <div
             style={{
               height: "47vh",
