a060b2ffd95ddcdd57c3032457d04cd921d5cdca
ContonJon <jonathanstenkilde@hotmail.com>
2023-03-28 14:44:12 +0200

    Changed chart into bars

--- src/pages/Dashboard/components/SummaryChart.tsx
+++ src/pages/Dashboard/components/SummaryChart.tsx
@@ -1,6 +1,6 @@
 import {
-  Area,
-  AreaChart,
+  Bar,
+  BarChart,
   CartesianGrid,
   Legend,
   Tooltip,
@@ -32,7 +32,7 @@ export const SummaryChart = ({ data, filter, w, h }: SummaryChartProps) => {
   const { t } = useTranslation();
 
   return (
-    <AreaChart
+    <BarChart
       width={w}
       height={h}
       data={data}
@@ -45,40 +45,40 @@ export const SummaryChart = ({ data, filter, w, h }: SummaryChartProps) => {
     >
       <defs>
         <linearGradient id="colorTotal" x1="0" x2="0" y1="0" y2="1">
-          <stop offset="5%" stopColor="#2451B7" stopOpacity={0.4} />
-          <stop offset="75%" stopColor="#2451B7" stopOpacity={0.05} />
+          <stop offset="5%" stopColor="#2451B7" stopOpacity={0.5} />
+          <stop offset="75%" stopColor="#2451B7" stopOpacity={0.7} />
         </linearGradient>
         <linearGradient id="colorFemale" x1="0" x2="0" y1="0" y2="1">
-          <stop offset="5%" stopColor="#b72424" stopOpacity={0.4} />
-          <stop offset="75%" stopColor="#b72424" stopOpacity={0.05} />
+          <stop offset="5%" stopColor="#b72424" stopOpacity={0.5} />
+          <stop offset="75%" stopColor="#b72424" stopOpacity={0.7} />
         </linearGradient>
         <linearGradient id="colorMale" x1="0" x2="0" y1="0" y2="1">
-          <stop offset="5%" stopColor="#24b75f" stopOpacity={0.4} />
-          <stop offset="75%" stopColor="#24b75f" stopOpacity={0.05} />
+          <stop offset="5%" stopColor="#24b75f" stopOpacity={0.5} />
+          <stop offset="75%" stopColor="#24b75f" stopOpacity={0.7} />
         </linearGradient>
       </defs>
-      <Area
+
+      <Bar
         type="monotone"
         dataKey={filter === "sex" ? "male" : ""}
         stackId="1"
         stroke="#24b75f"
         fill="url(#colorMale)"
       />
-      <Area
+      <Bar
         type="monotone"
         dataKey={filter === "sex" ? "female" : "lgbtq"}
         stackId="1"
         stroke="#b72424"
         fill="url(#colorFemale)"
       />
-      <Area
+      <Bar
         type="monotone"
         dataKey="total"
         stackId="1"
         stroke="#2451B7"
         fill="url(#colorTotal)"
       />
-
       <CartesianGrid strokeDasharray="2 3" opacity={0.1} vertical={false} />
       <XAxis
         dataKey="country"
@@ -96,6 +96,6 @@ export const SummaryChart = ({ data, filter, w, h }: SummaryChartProps) => {
           i18n.language === "english" ? value : t(value)
         }
       />
-    </AreaChart>
+    </BarChart>
   );
 };
