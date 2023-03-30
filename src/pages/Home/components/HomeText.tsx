7994c84ed7939decf8bdcf73561e8fc71ef689f2
Lily Gostovic <lily.gostovic@gmail.com>
2023-03-30 18:35:48 +0200

    Design bottom half of home page

--- src/pages/Home/components/HomeText.tsx
+++ src/pages/Home/components/HomeText.tsx
@@ -1,34 +1,23 @@
 import React from "react";
-import styled from "styled-components";
+import { StyledDiv } from "../../../components/common/StyledDiv";
+import { StyledText } from "../../../components/common/StyledText";
 
-// todo:: add actual text here
+interface HomeTextBlockProps {
+  title: string;
+  text: string;
+}
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  padding: 0px 200px;
-`;
-
-export const HomeText = () => (
-  <Container>
-    <h1>About This Project</h1>
-    <div>
-      Here we could maybe add reasons for why this is the best project ever
-    </div>
-    <h1>What YOU have to do</h1>
-    <div>
-      Maybe instructions for what the user can do??? What each page shows, and
-      how to operate them?
-    </div>
-    <h1>Resources</h1>
-    <div>The website/tools we have used? What kind of data is used?</div>
-    <h1>Contact Us</h1>
-    <div>
-      Contact information like emails and some links perhaps? Maybe some info
-      like this could also be placed in the empty bottom right corner? IDK what
-      else could be interesting to place in the empty corner? :DDD
-    </div>
-  </Container>
+export const HomeTextBlock = ({ title, text }: HomeTextBlockProps) => (
+  <StyledDiv
+    display="flex"
+    flexDirection="column"
+    alignItems="center"
+    mx="200px"
+    py="40px"
+  >
+    <StyledText variant="headerBig">{title}</StyledText>
+    <StyledText variant="paragraphMedium" textAlign="center">
+      {text}
+    </StyledText>
+  </StyledDiv>
 );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
-import { HomeText } from "./components";
+import { HomeTextBlock } from "./components/HomeText";
 import { Nav } from "../../components";
+import { StyledDiv } from "../../components/common/StyledDiv";
 import { TitleBlock } from "./components/TitleBlock";
 import { data } from "../Dashboard/data";
 import { useTranslation } from "react-i18next";
@@ -16,15 +17,30 @@ export const Home = () => {
   return (
     <div>
       <Nav />
-      <div>
+      <StyledDiv mb="100px">
         <TitleBlock
           title={title}
           subtitle={subtitle}
           data={data}
           filter={filter}
         />
-        <HomeText />
-      </div>
+        <HomeTextBlock
+          title="About This Project"
+          text="Here we could maybe add reasons for why this is the best project ever"
+        />
+        <HomeTextBlock
+          title="What YOU have to do"
+          text="Maybe instructions for what the user can do??? What each page shows, and how to operate them?"
+        />
+        <HomeTextBlock
+          title="Resources"
+          text="The website/tools we have used? What kind of data is used?"
+        />
+        <HomeTextBlock
+          title="Contact Us"
+          text="Contact information like emails and some links perhaps? Maybe some info like this could also be placed in the empty bottom right corner? IDK what else could be interesting to place in the empty corner? :DDD"
+        />
+      </StyledDiv>
     </div>
   );
 };
