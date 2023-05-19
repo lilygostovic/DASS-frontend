ad44e979ccb745bc6bce2ace64ff0c4470bc8582
Lily Gostovic <lily.gostovic@gmail.com>
2023-05-19 15:17:27 +0200

    center text on no results view

--- src/pages/Cases/NoResultView.tsx
+++ src/pages/Cases/NoResultView.tsx
@@ -2,21 +2,25 @@ import { StyledDiv } from "src/components/common/StyledDiv";
 import { StyledText } from "src/components/common/StyledText";
 import { noResult } from "src/images";
 
+// <div style={{ width: "400px", backgroundColor: "red", textAlign: "center" }}>
+//   <p>Result Not Found</p>
+// </div>
+
 export const NoResultView = () => (
   <StyledDiv justifyContent="center" width="350px">
     <StyledDiv height="250px" width="250px" margin="auto">
       <img src={noResult} height="250px" />
     </StyledDiv>
-    <StyledDiv>
+    <div style={{ textAlign: "center", marginBottom: "12px" }}>
       <StyledText variant="paragraphMediumBold" mb="20px">
         Result Not Found
       </StyledText>
-    </StyledDiv>
-    <StyledDiv>
-      <StyledText variant="paragraphSmallBold" color="darkGrey">
+    </div>
+    <div style={{ textAlign: "center" }}>
+      <StyledText variant="paragraphSmall" color="darkGrey">
         Unfortunately no cases match your search. Please try again with
         different search values.
       </StyledText>
-    </StyledDiv>
+    </div>
   </StyledDiv>
 );
